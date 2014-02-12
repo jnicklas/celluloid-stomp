8bd13c4d562f086eb8ce3235fe92fa7fcd67babf
Jonas Nicklas and Kim Burgestrand <dev+jnicklas+burgestrand@elabs.se>
2014-02-12 09:23:35 +0100

    initial commit

---
+++ lib/celluloid/stomp.rb
@@ -0,0 +1,7 @@
+require "celluloid/stomp/version"
+
+module Celluloid
+  module Stomp
+    # Your code goes here...
+  end
+end
---
+++ lib/celluloid/stomp/version.rb
@@ -0,0 +1,5 @@
+module Celluloid
+  module Stomp
+    VERSION = "0.0.1"
+  end
+end
